bf3389b3daba635600929bbfefb661188f048524
Sergio Bruno <sergio.bruno.ti@gmail.com>
2021-03-06 19:52:43 -0300

    Implementando o servidor e cliente Eureka

---
+++ eureka-service/src/test/java/com/slbruno/microservice/eureka/EurekaServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.slbruno.microservice.eureka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
--- loja/src/main/java/com/slbruno/microservice/loja/service/CompraService.java
+++ loja/src/main/java/com/slbruno/microservice/loja/service/CompraService.java
@@ -1,26 +1,29 @@
 package com.slbruno.microservice.loja.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
 import com.slbruno.microservice.loja.controller.dto.CompraDTO;
-import com.slbruno.microservice.loja.controller.dto.EnderecoDTO;
 import com.slbruno.microservice.loja.controller.dto.InfoFornecedorDTO;
 
 @Service
 public class CompraService {
 
+	@Autowired
+	private RestTemplate client;
+	
 	public void realizaCompra(CompraDTO compra) {
 		
-		EnderecoDTO endereco = new EnderecoDTO();
-		endereco.setEstado("PE");
-		compra.setEndereco(endereco); 
+		/*
+		 * EnderecoDTO endereco = new EnderecoDTO(); endereco.setEstado("PE");
+		 * compra.setEndereco(endereco);
+		 */
 		
-		RestTemplate client = new RestTemplate();
 		ResponseEntity<InfoFornecedorDTO> exchange =
-				client.exchange("http://localhost:8081/info/"+compra.getEndereco().getEstado(),
+				client.exchange("http://fornecedor/info/"+compra.getEndereco().getEstado(),
 				HttpMethod.GET, null, InfoFornecedorDTO.class);
 		
 		System.out.println(exchange.getBody().getEndereco());
